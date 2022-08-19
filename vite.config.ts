b19755176394bd384adaba41b191b0c4ec11cc10
Justin <justin@nextenergytech.com>
2022-08-18 17:58:47 -0700

    Comment out potentially helpful build settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import path from "path";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+});
+
+// export default defineConfig({
+//   build: {
+//     lib: {
+//       entry: path.resolve(__dirname, "src/components/index.js"),
+//       name: "JSME-vue",
+//       fileName: (format) => `jsme-vue.${format}.js`,
+//     },
+//     rollupOptions: {
+//       external: ["vue"],
+//       output: {
+//         // Provide global variables to use in the UMD build
+//         // Add external deps here
+//         globals: {
+//           vue: "Vue",
+//         },
+//       },
+//     },
+//   },
+//   plugins: [vue()],
+// });
