40dd6130d482b8418e1257823df9a797ef2f2245
Justin <justin@nextenergytech.com>
2022-08-18 18:13:22 -0700

    bump build and version

--- vite.config.ts
+++ vite.config.ts
@@ -3,27 +3,27 @@ import path from "path";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-});
-
 // export default defineConfig({
-//   build: {
-//     lib: {
-//       entry: path.resolve(__dirname, "src/components/index.js"),
-//       name: "JSME-vue",
-//       fileName: (format) => `jsme-vue.${format}.js`,
-//     },
-//     rollupOptions: {
-//       external: ["vue"],
-//       output: {
-//         // Provide global variables to use in the UMD build
-//         // Add external deps here
-//         globals: {
-//           vue: "Vue",
-//         },
-//       },
-//     },
-//   },
 //   plugins: [vue()],
 // });
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/components/index.ts"),
+      name: "jsme-vue",
+      fileName: (format) => `jsme-vue.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // Add external deps here
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+  plugins: [vue()],
+});
